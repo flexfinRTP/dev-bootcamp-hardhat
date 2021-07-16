e2ab573b36906fda377546555c4d3ab91c8b5f0c
flexfinRTP <gaffney311@outlook.com>
2021-07-16 18:50:31 -0500

    votingConsumer

---
+++ deploy/05_Deploy_VotingCosumer.js
@@ -0,0 +1,16 @@
+//take in params from VotingConsumer.sol and add to this script.
+
+//the format you need to pass into the constructor in your deploy script needs to be an array of byte32 values
+    
+// let name1 = ethers.utils.formatBytes32String('harry')
+//   let name2 = ethers.utils.formatBytes32String('josh')
+//   console.log(name1 + ' ' + name2)
+//   var myData = [name1, name2];
+
+//   const ballot = await deploy('Ballot', {
+//     from: deployer,
+//     args: [myData],
+//     log: true
+//   })
+
+//you can manipulate and setup the array as you wish, just ensure that the values are setup in the correct format, eg '0x6861727279000000000000000000000000000000000000000000000000000000'
