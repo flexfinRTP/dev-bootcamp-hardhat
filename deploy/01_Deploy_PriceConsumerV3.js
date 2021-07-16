14f875b18c1f5e031c19dc4e6972df10da9d5773
flexfinRTP <gaffney311@outlook.com>
2021-07-16 13:09:22 -0500

    Developer Bootcamp

--- deploy/01_Deploy_PriceConsumerV3.js
+++ deploy/01_Deploy_PriceConsumerV3.js
@@ -14,8 +14,10 @@ module.exports = async ({
         const EthUsdAggregator = await deployments.get('EthUsdAggregator')
         ethUsdPriceFeedAddress = EthUsdAggregator.address
     } else {
-        ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed']
+    ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed']
     }
+ 
+
     // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     // Default one below is ETH/USD contract on Kovan
     log("----------------------------------------------------")
